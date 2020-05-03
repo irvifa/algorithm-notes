29d54c2eae5c05f0b924303cf66c1042a94e8a31
Irvi Firqotul Aini <irvi.fa@gmail.com>
2020-05-03 20:29:02 +0700

    Fix build error due to checkstyle no parentheses after if.

--- src/main/java/com/github/irvifa/algorithmnotes/leetcode/tree/pathsum/Solution.java
+++ src/main/java/com/github/irvifa/algorithmnotes/leetcode/tree/pathsum/Solution.java
@@ -4,10 +4,12 @@ import com.github.irvifa.algorithmnotes.leetcode.tree.TreeNode;
 
 public class Solution {
   public boolean hasPathSum(TreeNode root, int sum) {
-    if (root == null)
+    if (root == null) {
       return false;
-    if (root.val == sum && (root.left == null && root.right == null))
+    }
+    if (root.val == sum && (root.left == null && root.right == null)) {
       return true;
+    }
 
     return hasPathSum(root.left, sum - root.val)
         || hasPathSum(root.right, sum - root.val);
